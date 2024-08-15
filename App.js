04bfb62f195793235b7ae53da91c319c8f1bb08e
letzho <tzhoji@gmail.com>
2024-08-16 00:27:51 +0800

    rename

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StatusBar,StyleSheet, Text, View,SafeAreaView,Platform } from 'react-native';
 
+const isAndroid=Platform.OS==='android';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <SafeAreaView style={styles.safeview}>
+      <View style={styles.searchcontainer}>
+        <Text>Search</Text>
+      </View>
+      <View style={styles.container}>
+        <Text>try now</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  safeview:{
+    flex:1,
+    marginTop:StatusBar.currentHeight
+  },
+  searchcontainer:{
+    padding:16,
+    backgroundColor:'green'
+  },
   container: {
-    flex: 1,
+    flex:1,
+    
+    padding:16,
+    // alignItems:'right',
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
